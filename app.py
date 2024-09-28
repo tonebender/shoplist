a3773806d9e61c14c00eb37d4e4da453d0b18546
Henrik Lidbjörk <henrik.lidbjork@gmail.com>
2024-09-28 23:47:09 +0200

    First commit. Beginnings of python backend.

---
+++ app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+app.config.update(SAVEDIR='shoplists')
+
+
+@app.route('/')
+def indexpage():
+    return render_template('index.html')
+
+
+@app.route('/save/<name>', methods=['POST'])
+def save_shoplist(name):
+    content = request.get_json()
+    return 'You entered ' + secure_filename(name) + '.json'
+
+
+if __name__ == '__main__':
+    app.run()
